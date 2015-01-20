9df0c2e35f84bf6737cea4f8397f1eb5c7c20959
louisjohnbichard <louisjohnbichard@gmail.com>
2015-01-20 15:45:27 +0000

    silent cc

--- scripts/controllers/developerConsoleController.js
+++ scripts/controllers/developerConsoleController.js
@@ -2,59 +2,59 @@
 // PARTIAL: DEVELOPER_CONSOLE
 
 SU.controller('developerConsoleController', function($scope, apiService, chartService) {
-    $scope.results = {
-        backend: {},
-        frontend: {}
-    };
-    $scope.loading = {
-        frontend: false,
-        backend: false
-    };
-    $scope.frontend_tests = {};
-    $scope.errors_by = {};
-
-    // CHART CONFIGURATIONS
-    $scope.chart_config = {
-        "labels": false,
-        "legend": {
-            "display": false,
-            "position": "right"
-        },
-        "lineLegend": false
-    };
-
-    $scope.runTest = function(type) {
-        $scope.loading[type] = true;
-        apiService.get('/test' + type, null, {
-                preventNotifications: true
-            })
-            .then(_.partialRight($scope.addResponseToScope, 'type'));
-    };
-
-    $scope.addResponseToScope = function(result, type) {
-        $scope.$apply(function() {
-            $scope.results[type] = result;
-            $scope.loading[type] = false;
-        });
-    };
-
-    $scope.addErrorsToScope = function(result) {
-        $scope.errors_by = {
-            'anonymous': chartService.formatErrors(_.countBy(result, 'anonymous')),
-            'err': chartService.formatErrors(_.countBy(result, 'err')),
-            'location': chartService.formatErrors(_.countBy(result, 'location'))
+    /*    $scope.results = {
+            backend: {},
+            frontend: {}
         };
-        $scope.error_list = result;
-    };
+        $scope.loading = {
+            frontend: false,
+            backend: false
+        };
+        $scope.frontend_tests = {};
+        $scope.errors_by = {};
 
-    $scope.getErrors = function() {
-        apiService.get('/backenderrors', null, {
-                preventNotifications: true
-            })
-            .then($scope.addErrorsToScope);
-    };
+        // CHART CONFIGURATIONS
+        $scope.chart_config = {
+            "labels": false,
+            "legend": {
+                "display": false,
+                "position": "right"
+            },
+            "lineLegend": false
+        };
 
-    $scope.error_data = $scope.getErrors();
+        $scope.runTest = function(type) {
+            $scope.loading[type] = true;
+            apiService.get('/test' + type, null, {
+                    preventNotifications: true
+                })
+                .then(_.partialRight($scope.addResponseToScope, 'type'));
+        };
+
+        $scope.addResponseToScope = function(result, type) {
+            $scope.$apply(function() {
+                $scope.results[type] = result;
+                $scope.loading[type] = false;
+            });
+        };
+
+        $scope.addErrorsToScope = function(result) {
+            $scope.errors_by = {
+                'anonymous': chartService.formatErrors(_.countBy(result, 'anonymous')),
+                'err': chartService.formatErrors(_.countBy(result, 'err')),
+                'location': chartService.formatErrors(_.countBy(result, 'location'))
+            };
+            $scope.error_list = result;
+        };
+
+        $scope.getErrors = function() {
+            apiService.get('/backenderrors', null, {
+                    preventNotifications: true
+                })
+                .then($scope.addErrorsToScope);
+        };
+
+        $scope.error_data = $scope.getErrors();*/
 
     /*apiService.get('/codecomplexity')
         .then(function(result) {
