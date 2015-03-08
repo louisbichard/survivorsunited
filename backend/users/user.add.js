2c6d11540a3052ee0830d64b75e78386caa78716
louisjohnbichard <louisjohnbichard@gmail.com>
2015-03-08 17:46:06 +0000

    added email integrations

--- backend/users/user.add.js
+++ backend/users/user.add.js
@@ -1,14 +1,9 @@
-//ENDPOINT /user/add
-
-// TODO: ADD TESTS
-// 
-// 1) ADD CHECK FOR SUCCESS MESSAGE
-
 var _ = require('lodash');
 var Promise = require('bluebird');
 var MongoClient = Promise.promisifyAll(require("mongodb")).MongoClient;
 var db = require('../utilities/database.js');
 var log = require('../utilities/logger.js');
+var email = require('../utilities/utilities.email.js');
 
 module.exports = function(req, res) {
 
@@ -27,9 +22,22 @@ module.exports = function(req, res) {
         severity_grade: "Low"
     };
 
+    var send_confirmation_email = function(result) {
+        return email.sendEmail({
+            'subject': 'Welcome to Survivors United ' + result[0].username,
+            'template': 'backend/email/templates/welcome.html',
+            'to': ['contact@louisjohnbichard.co.uk'] // TODO: Send this to the email specified
+        });
+    };
+
     return db.insert('users', [_.extend(data_to_insert, post_params)])
         .then(function(result) {
+            // TODO: ASSIGN TASKS TO THIS ID
             var user_id = result._id;
+            return result;
+        })
+        .then(send_confirmation_email)
+        .then(function() {
             return respond.success("User " + post_params.username + ' added');
         })
         .caught(function(err) {
