8bafb678ee0f0d0f660d2c2d68c07a44f85a165c
louisjohnbichard <louisjohnbichard@gmail.com>
2015-03-11 14:37:47 +0000

    added dependency script

--- scripts/controllers/process.create.controller.js
+++ scripts/controllers/process.create.controller.js
@@ -1,6 +1,15 @@
 SU.controller('createProcessController', function($scope, apiService, utilityService, notifyService) {
 
     $scope.test = "test_test process create controller";
+
+    $scope.new_task = {};
+
+    $scope.addTaskToProcess = function() {
+        $scope.process.tasks.push($scope.new_task);
+        $scope.new_task = {};
+        notifyService.success('Added task to process');
+    };
+
     $scope.process = {
         "_id": "123 b2323b3b3b3b3332",
         "name": "some process name",
@@ -9,14 +18,23 @@ SU.controller('createProcessController', function($scope, apiService, utilitySer
         "date_created": 1425826243690,
         "date_updated": 1425826243690,
         "tasks": [{
-            "id": "some_id",
+            "id": "some_id1",
             "name": "Some task name",
             "description": "test",
             "content": "<p> Some html </p>",
             "dependencies": [
-                "task_id",
-                "task_id"
+                "some_id2"
             ],
+            "completed_by": [
+                "user_id",
+                "user_id"
+            ]
+        }, {
+            "id": "some_id2",
+            "name": "Some task name",
+            "description": "test",
+            "content": "<p> Some html </p>",
+            "dependencies": [],
             "completed_by": [
                 "user_id",
                 "user_id"
@@ -24,4 +42,5 @@ SU.controller('createProcessController', function($scope, apiService, utilitySer
         }],
         "assignees": ["b1233b2k2kj2j2"]
     };
+
 });
--- scripts/directives/process.create.directive.js
+++ scripts/directives/process.create.directive.js
@@ -1,83 +1,111 @@
 SU.directive('d3CreateProcess', function() {
     return {
         restrict: 'EA',
-        scope: {},
+        scope: {
+            tasks: '=',
+        },
         link: function(scope, element, attrs, $window) {
 
-            var canvas = d3.select(element[0])
-                .append('svg')
-                .attr("width", '100%')
-                .style('background-color', 'red')
-                .attr("height", '100%');
+            var init = function() {
 
-            var drag = d3.behavior.drag().on('dragstart', function() {
-                    d3.select(this.children[0]).style('fill', 'yellow');
-                })
-                .on('drag', function() {
-                    d3.select(this)
-                        .attr("transform", "translate(" +
-                        (d3.event.x - this.children[0].attributes.cx.value) + "," + (d3.event.y - this.children[0].attributes.cy.value) + ")");
-                })
-                .on('dragend', function() {
-                    d3.select(this.children[0]).style('fill', 'purple');
-                });
+                var canvas = d3.select(element[0])
+                    .html('') // Reset the canvas so that it redraws on init command
+                    .append('svg')
+                    .attr("width", '100%')
+                    .style('background-color', 'red')
+                    .attr("height", '100%');
 
-            var tasks = [{
-                id: 1,
-                pos: 0
-            }, {
-                id: 2,
-                pos: 100
-            }, {
-                id: 3,
-                pos: 200
-            }];
-
-            canvas.append("line")
-                .style("stroke", "black")
-                .attr("x1", 150)
-                .attr("y1", 100)
-                .attr("x2", 250)
-                .attr("y2", 300);
-
-            var map = _.each(tasks, function(curr) {
-
-                var g = canvas.append('g')
-                .attr('id', 'task-' + curr.id)
-                .call(drag);
-
-                g.insert("circle")
-                    .attr("cx", 50 + curr.pos)
-                    .attr("cy", 50)
-                    .attr("r", 50).style("fill", "purple");
-
-                g.append('text')
-                    .style("fill", "white")
-                    .attr("dx", 50 + curr.pos)
-                    .attr("dy", 50)
-                    .attr("text-anchor", "middle")
-                    .text(function(d) {
-                        return "test" + curr.id;
+                var drag = d3.behavior.drag().on('dragstart', function() {
+                        d3.select(this.children[0]).style('fill', 'yellow');
+                    })
+                    .on('drag', function() {
+                        d3.select(this)
+                            .attr("transform",
+                                "translate(" +
+                                // ADD CORDINATES OF THE CIRCLE INSIDE THE G(GROUP) ELEMENT
+                                (d3.event.x - this.children[0].attributes.cx.value) +
+                                "," +
+                                (d3.event.y - this.children[0].attributes.cy.value) +
+                                ")"
+                            );
+                    })
+                    .on('dragend', function() {
+                        d3.select(this.children[0]).style('fill', 'purple');
                     });
 
-            });
+
+                var inc = 0;
+                // CREATE GROUPINGS AND CIRCLES WITH LABELS
+                _.each(scope.tasks, function(curr) {
+
+                    var g = canvas.append('g')
+                        .attr('id', 'task-' + curr.id)
+                        .call(drag);
+
+                    g.insert("circle")
+                        .attr("cx", 50 + inc)
+                        .attr("cy", 50)
+                        .attr("r", 50)
+                        .style("fill", "purple");
+
+
+
+                    g.append('text')
+                        .style("fill", "white")
+                        .attr("dx", 50 + inc)
+                        .attr("dy", 50)
+                        .attr("text-anchor", "middle")
+                        .text(function(d) {
+                            return curr.id;
+                        });
+
+                    inc = inc + 150;
+                });
+
+                // CREATE GROUPINGS AND CIRCLES WITH LABELS
+                _.each(scope.tasks, function(curr) {
+
+                    var start_node = canvas.select('#task-' + curr.id)[0][0].children[0];
+
+                    var start_x = start_node.cx.baseVal.value;
+                    var start_y = start_node.cy.baseVal.value;
+                    console.log(curr.id);
+
+                    _.each(curr.dependencies, function(id) {
+
+                        var end_node = canvas.select('#task-' + id)[0][0].children[0];
+
+                        var end_x = end_node.cx.baseVal.value;
+                        var end_y = end_node.cy.baseVal.value;
+
+                        // Draw line between dependent nodes
+                        canvas.append("line")
+                            .style("stroke", "black")
+                            .style("stroke-width", "5")
+                            .attr("x1", start_x)
+                            .attr("y1", start_y)
+                            .attr("x2", end_x)
+                            .attr("y2", end_y);
+                    });
+
+
+
+                });
+            };
 
             // Browser onresize event
             window.onresize = function() {
                 scope.$apply();
             };
 
-            // TODO: Watch for resize event
-            scope.$watch(function() {
-                //
-            }, function() {
-                scope.render(scope.data);
-            });
+            // TODO: Watch for resize event  scope.render(scope.data);
 
-            scope.render = function(data) {
-                console.log('d3 rendering');
-                // our custom d3 code
-            };
+            scope.$watch('tasks', function() {
+                init();
+                console.log('init because tasks reset');
+            }, true);
+
+            init(); // run setup at the start
         }
     };
 });
