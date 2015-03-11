c10a26b9046c5c27892a5e24bfa46fecc5ee9456
louisjohnbichard <louisjohnbichard@gmail.com>
2015-03-11 16:21:03 +0000

    working with dragging lines

--- scripts/directives/process.create.directive.js
+++ scripts/directives/process.create.directive.js
@@ -13,99 +13,134 @@ SU.directive('d3CreateProcess', function() {
                 .style('background-color', 'red')
                 .attr("height", '100%');
 
-            var init = function() {
-                var drag = d3.behavior.drag().on('dragstart', function() {
-                        d3.select(this.children[0]).style('fill', 'yellow');
-                    })
-                    .on('drag', function() {
-                        createLines();
-                        d3.select(this)
-                            .attr("transform",
-                                "translate(" +
-                                // ADD CORDINATES OF THE CIRCLE INSIDE THE G(GROUP) ELEMENT
-                                (d3.event.x - this.children[0].attributes.cx.value) +
-                                "," +
-                                (d3.event.y - this.children[0].attributes.cy.value) +
-                                ")"
-                            );
-                    })
-                    .on('dragend', function() {
-                        d3.select(this.children[0]).style('fill', 'purple');
-                    });
+            var process_designer = {
+                "init": function() {
+                    this.destroy.canvas();
+                    this.drawNodes();
+                    this.drawLines();
+                },
+                destroy: {
+                    canvas: function() {
+                        return canvas.html('');
+                    },
+                    all_lines: function() {
+                        return canvas.selectAll('.dependency-line').remove();
+                    }
+                },
+                group: {
+                    // NOTE: PASSING IN PROCESS DESIGNER LIKE THIS IS A LITTLE HACKY
+                    create: function(process_designer, id) {
+                        return canvas.append('g')
+                            .attr('id', 'task-' + id)
+                            .call(process_designer.handlers.drag.run());
+                    },
+                    insertCircle: function(g, inc) {
+                        return g.insert("circle")
+                            .attr("cx", 50 + inc)
+                            .attr("cy", 50 + inc)
+                            .attr("r", 50)
+                            .style("fill", "purple");
+                    },
+                    addLabel: function(g, inc, text) {
+                        return g.append('text')
+                            .style("fill", "white")
+                            .attr("dx", 50 + inc)
+                            .attr("dy", 50 + inc)
+                            .attr("text-anchor", "middle")
+                            .text(function(d) {
+                                return text;
+                            });
+                    }
+                },
+                handlers: {
+                    drag: {
+                        moveElementToMouseLocation: function(element) {
+                            return d3.select(element)
+                                .attr("transform",
+                                    "translate(" +
+                                    // ADD CORDINATES OF THE CIRCLE INSIDE THE G(GROUP) ELEMENT
+                                    (d3.event.x - element.children[0].attributes.cx.value) +
+                                    "," +
+                                    (d3.event.y - element.children[0].attributes.cy.value) +
+                                    ")"
+                                );
+                        },
+                        run: function() {
 
+                            var drag_handler = this;
 
-                var inc = 0;
-                // CREATE GROUPINGS AND CIRCLES WITH LABELS
-                _.each(scope.tasks, function(curr) {
+                            return d3.behavior.drag().on('dragstart', function() {
+                                    d3.select(this.children[0]).style('fill', 'yellow');
+                                })
+                                .on('drag', function() {
+                                    process_designer.destroy.all_lines();
+                                    process_designer.drawLines();
+                                    var element = this;
 
-                    var g = canvas.append('g')
-                        .attr('id', 'task-' + curr.id)
-                        .call(drag);
+                                    drag_handler.moveElementToMouseLocation(element);
 
-                    g.insert("circle")
-                        .attr("cx", 50 + inc)
-                        .attr("cy", 50 + inc)
-                        .attr("r", 50)
-                        .style("fill", "purple");
-
-
-
-                    g.append('text')
-                        .style("fill", "white")
-                        .attr("dx", 50 + inc)
-                        .attr("dy", 50 + inc)
-                        .attr("text-anchor", "middle")
-                        .text(function(d) {
-                            return curr.id;
-                        });
-
-                    inc = inc + 150;
-                });
-            };
-
-            var createLines = function() {
-                canvas.selectAll('.dependency-line').remove();
-
-                _.each(scope.tasks, function(curr) {
-
-                    var g = canvas.select('#task-' + curr.id);
-                    var circle = g[0][0].children[0];
-
+                                    // TODO: ADD MODULAR CONFIGURATION OF THIS TASK PREFIX
+                                    var id = this.id.split('task-')[1]; // remove task prefix
+                                    var scope_index = _.findIndex(scope.tasks, {'id': id});
+                                    var coordinates = process_designer.getGroupNodeLocation(d3.select(this));
+                                    scope.tasks[scope_index].coordinates = coordinates;
+                                    scope.tasks = _.clone(scope.tasks);
+                                    
+                                })
+                                .on('dragend', function() {
+                                    d3.select(this.children[0]).style('fill', 'purple');
+                                });
+                        }
+                    }
+                },
+                getGroupNodeLocation: function(group) {
+                    var circle = group[0][0].children[0];
                     var circle_x = circle.cx.baseVal.value;
                     var circle_y = circle.cy.baseVal.value;
+                    var g_x = d3.transform(group.attr("transform")).translate[0];
+                    var g_y = d3.transform(group.attr("transform")).translate[1];
+                    return {
+                        y: circle_y + g_y,
+                        x: circle_x + g_x
+                    };
+                },
+                drawLines: function() {
+                    var process_designer = this;
+                    _.each(scope.tasks, function(curr) {
+                        var node = canvas.select('#task-' + curr.id);
+                        var start = process_designer.getGroupNodeLocation(node);
+                        _.each(curr.dependencies, function(id) {
+                            var node = canvas.select('#task-' + id);
+                            var end = process_designer.getGroupNodeLocation(node);
 
-                    var g_x = d3.transform(g.attr("transform")).translate[0];
-                    var g_y = d3.transform(g.attr("transform")).translate[1];
-
-                    var start_x = circle_x + g_x;
-                    var start_y = circle_y + g_y;
-
-                    _.each(curr.dependencies, function(id) {
-
-                        var g = canvas.select('#task-' + id);
-                        var circle = g[0][0].children[0];
-
-                        var circle_x = circle.cx.baseVal.value;
-                        var circle_y = circle.cy.baseVal.value;
-
-                        var g_x = d3.transform(g.attr("transform")).translate[0];
-                        var g_y = d3.transform(g.attr("transform")).translate[1];
-
-                        var end_x = circle_x + g_x;
-                        var end_y = circle_y + g_y;
-
-                        // Draw line between dependent nodes
-                        canvas.append("line")
-                            .classed('dependency-line', true)
-                            .style("stroke", "black")
-                            .style("stroke-width", "5")
-                            .attr("x1", start_x)
-                            .attr("y1", start_y)
-                            .attr("x2", end_x)
-                            .attr("y2", end_y);
+                            // Draw line between dependent nodes
+                            canvas.append("line")
+                                .classed('dependency-line', true)
+                                .style("stroke", "black")
+                                .style("stroke-width", "5")
+                                .attr("x1", start.x)
+                                .attr("y1", start.y)
+                                .attr("x2", end.x)
+                                .attr("y2", end.y);
+                        });
                     });
-                });
-            }
+                },
+                drawNodes: function() {
+                    var process_designer = this;
+                    // USED TO SPREAD OUT THE NODES
+                    var inc = 0;
+
+                    // CREATE A GROUP WITH A CIRCLE AND LABEL
+                    _.each(scope.tasks, function(curr) {
+                        var g = process_designer.group.create(process_designer, curr.id);
+                        process_designer.group.insertCircle(g, inc);
+                        process_designer.group.addLabel(g, inc, curr.id);
+
+                        // SPREAD OUT THE NODES ON LOAD
+                        inc = inc + 150;
+                    });
+                }
+            };
 
             // Browser onresize event
             window.onresize = function() {
@@ -115,8 +150,7 @@ SU.directive('d3CreateProcess', function() {
             // TODO: Watch for resize event  scope.render(scope.data);
 
             scope.$watch('tasks', function() {
-                init();
-                createLines();
+                process_designer.init();
                 console.log('init because tasks reset');
             }, true);
 
